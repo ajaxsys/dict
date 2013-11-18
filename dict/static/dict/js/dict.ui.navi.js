ad3f575d9b5cd76af0fe6e79d049dd81d9ad2b27
Fang Dehui <javaeecoding@gmail.com>
2013-11-19 00:57:42 +0800

    bugs of navi.

--- dict/static/dict/js/dict.ui.navi.js
+++ dict/static/dict/js/dict.ui.navi.js
@@ -19,24 +19,23 @@ function initNavi(){
     //var on='\u2602',off='\u2604',
     //var on='☂',off='☄',
     var altOn='ON',altOff='OFF',
-        titleOn='SecondScreen Service ON',titleOff='SecondScreen Service OFF',
         classOn='__navi_on__',classOff='__navi_off__',
         $navi = $('<div style="position:fixed;top:0;left:0;z-index:2147483647;" class="__navi_div__"></div>'),
-        $img = $('<img>').attr('src','http://www.w3schools.com/css/img_trans.gif'),
-        $imgOn = $img.clone().addClass(classOn).prop('title',titleOn).prop('alt',altOn),
-        $imgOff = $img.clone().addClass(classOff).prop('title',titleOff).prop('alt',altOff);
+        $img = $('<img src="http://www.w3schools.com/css/img_trans.gif">').addClass(classOn).prop('alt',altOn);
 
-    $navi.append($imgOn);
+    $navi.append($img);
     $navi.click(function(){
-        if ($('img',$navi)[0]===$imgOn[0]){
-            $navi.empty().append($imgOff);
+        $img = $('img',$navi);
+        console.log($img.prop('alt')===altOn);
+        if ($img.prop('alt')===altOn){
+            $img.prop('alt',altOff).removeClass(classOn).addClass(classOff);
             
             // For next start up
-            DICT.DICT_SERVICE=false;
+            DICT.DICT_SERVICE=false; 
             $.closeWindow(DICT.DICT_ID);
             //$(document).off('mouseenter.plaintext').off('mouseleave.plaintext');
         } else {
-            $navi.empty().append($imgOn);
+            $img.prop('alt',altOn).removeClass(classOff).addClass(classOn);
             DICT.DICT_SERVICE=true;
             //$(document).on('mouseenter.plaintext').on('mouseleave.plaintext');
         }
